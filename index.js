2c8f9a147125ed4d5282b22ca2dccf36298f2ee0
Heymun Pareek <78228395+PassionateCoder340@users.noreply.github.com>
2025-05-22 19:53:12 +0530

    First Nodejs project

---
+++ index.js
@@ -0,0 +1,8 @@
+const index2 = require('./index2');
+
+let a = 1;
+let b = 2;
+let c = index2.add(a, b);
+let d = index2.subtract(a, b);
+console.log(`The sum of ${a} and ${b} is ${c}`);
+console.log(`The difference of ${a} and ${b} is ${d}`);
